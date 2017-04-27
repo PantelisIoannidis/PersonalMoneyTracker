18121458760171ec3911640e943f69f9ca9abe2f
Pantelis Ioannidis <Pantelis.ioannidis@outlook.com>
2017-04-27 22:15:50 +0300

    First commit, basic bootstrap stuff

---
+++ PMT.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PMT.Web.MvcApplication" Language="C#" %>
---
+++ PMT.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PMT.Web.Startup))]
+namespace PMT.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
